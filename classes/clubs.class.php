6b1cc6ddec5f4f6bf5aea44675141df8bb3f8d5b
abdallah <abddoullah.bouzhar@gmail.com>
2022-11-20 00:35:03 +0100

    dddd 	modified:   classes/clubs.class.php
    	modified:   classes/clubsview.class.php

--- classes/clubs.class.php
+++ classes/clubs.class.php
@@ -10,6 +10,16 @@ class clubs extends Dbh {
         $results = $stmt->fetchAll();
         return $results;
     }
+    protected function getClubs(){
+        $sql = "select * from club";
+        $stmt = $this->connect()->prepare($sql);
+        $stmt->execute();
+
+        $results = $stmt->fetchAll();
+        return $results;
+    }
+ 
+
     protected function setClub($nom,$description,$datecreation,$logo){
         $sql = "insert into club (nom,description,date_creation,logo) values(?,?,?,?)";
         $stmt = $this->connect()->prepare($sql);
--- classes/clubsview.class.php
+++ classes/clubsview.class.php
@@ -2,8 +2,12 @@
 
 class ClubsView extends Clubs {
 
-    public function showUser($nom){
+    public function showClub($nom){
         $results = $this->getClub($nom);
         echo "club name:".$results[0]['nom']." ".$results[0]['membres'];
+    }
+
+    public function showClubs(){
+
     }
 }
